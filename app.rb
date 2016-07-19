0293721f945a8cacb1d372a71fdf93171d61bed6
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-07-19 12:14:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	
+	configure do
+		enable :sessions
+		set :session_secret, 'somesecret'
+	end
+
+	get '/' do
+		erb :index
+	end
+	post '/checkout' do
+		session[:item] = params[:item]
+		"#{ params[:item] }"
+	end
 end
