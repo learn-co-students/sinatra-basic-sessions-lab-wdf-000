24dc3380578ade5d42457d5ce0aa3af775c038fa
Flatiron School <github email address>
2016-08-20 14:02:08 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    # binding.pry
+    @item = Item.new(params[:item])
+    session[:item] = @item.name
+    @session = session
+    erb :purchase
+  end
+
+  configure do
+    enable :sessions
+  end
+
+
+
 end
