5f1b5ce542d5ae6e7f53dbbf0392725dce503fe7
Naveed Shah <naveed@tayybah.com>
2016-09-04 15:14:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+require 'pry'
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+  end
 end
