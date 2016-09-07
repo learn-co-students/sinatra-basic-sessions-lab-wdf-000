72a3c463a72e43269a9b25a7d1ae4bf163ea3711
Kelene Chung <simoy93@gmail.com>
2016-09-07 11:43:31 -0400

    Sinatra Basic Sessions Lab DONE

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
+require 'pry'
 require_relative 'config/environment'
 
+configure do 
+	enable :sessions
+	set :session_secret, "secret"
+end
+
+
 class App < Sinatra::Base
+
+	get '/' do 
+		erb :index
+	end
+
+	post '/checkout' do 
+		@session = session
+		@session[:item] = params[:item]
+		# binding.pry
+	end
+
 end
